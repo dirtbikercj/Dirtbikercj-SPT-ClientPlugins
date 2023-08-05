41c760bae3b35ab1aa107195606e9e607bc87fcf
dirtbikercj <dirtbikercj@gmail.com>
2023-08-05 01:44:50 -0400

    Add Game Settings Patch

---
+++ GameSettings/Patches/Patches.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT.UI;
+using HarmonyLib;
+
+
+namespace GameSettings
+{
+  
+    internal class GameSettingsPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() =>
+            typeof(SharedGameSettingsClass).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, binder: null, new Type[0], null);
+
+        [PatchPrefix]
+        static void GameSettingsPatchDynamic(SharedGameSettingsClass __instance)
+        {
+            string newSettingsPath = AppDomain.CurrentDomain.BaseDirectory + @"\BepInEx\plugins\GameSettings";
+            var string_0 = AccessTools.Field(typeof(SharedGameSettingsClass), "string_0");
+            var string_1 = AccessTools.Field(typeof(SharedGameSettingsClass), "string_1");
+
+            string_0.SetValue(__instance, newSettingsPath);
+            string_1.SetValue(__instance, newSettingsPath);
+        }
+    }
+
+
+    internal class GameSettingsPatchStatic : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() =>
+            typeof(SharedGameSettingsClass).GetConstructor(BindingFlags.Static | BindingFlags.NonPublic, binder: null, new Type[0], null);
+
+        [PatchPrefix]
+        static void GameSettingsStaticPatch()
+        {
+            string newSettingsPath = AppDomain.CurrentDomain.BaseDirectory + @"\BepInEx\plugins\GameSettings";
+            if (!Directory.Exists(newSettingsPath))
+            {
+                Directory.CreateDirectory(newSettingsPath);
+            }
+        }
+    }
+}
---
+++ GameSettings/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace GameSettings
+{
+    [BepInPlugin("com.dirtbikercj.gameSettings", "Game Settings patch", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal void Awake()
+        {
+            new GameSettingsPatch().Enable();
+            new GameSettingsPatchStatic().Enable();
+        }
+    }
+}
